b5de32b74a12f35475b8a03ab8a349946910771e
AmirMahdi <amirnovin80@gmail.com>
2020-07-31 18:27:22 +0430

    Edit SplashActivity

--- app/src/main/java/com/example/amirnovinfar/Activity/SpplashActivity.java
+++ app/src/main/java/com/example/amirnovinfar/Activity/SpplashActivity.java
@@ -2,13 +2,28 @@ package com.example.amirnovinfar.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.Display;
+
+import com.example.amirnovinfar.R;
 
 public class SpplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_spplash);
+        SharedPreferences sharedPreferences=getSharedPreferences("islogin", MODE_PRIVATE);
+        boolean islogin=sharedPreferences.getBoolean("ISLOGIN",false);
+        if (!islogin) {
+            startActivity(new Intent(SpplashActivity.this,MainActivity.class));
+            finish();
+        }else {
+            startActivity(new Intent(SpplashActivity.this,FlashLightActivity.class));
+            finish();
+
+        }
+
     }
 }
