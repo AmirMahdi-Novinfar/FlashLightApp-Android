4d28338311e4b8f3936dbf531f835de0060f21c7
AmirMahdi <amirnovin80@gmail.com>
2020-07-31 18:14:25 +0430

    Add Splash Activity

---
+++ app/src/main/java/com/example/amirnovinfar/Activity/SpplashActivity.java
@@ -0,0 +1,14 @@
+package com.example.amirnovinfar.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SpplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_spplash);
+    }
+}
