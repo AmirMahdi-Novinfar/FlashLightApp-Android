0721b04a0724eb20f2db3b1d1cc042a3ac239bc7
AmirMahdi <amirnovin80@gmail.com>
2020-08-01 16:39:23 +0430

    Config Turn ON or Turn Off FlashLight

--- app/src/main/java/com/example/amirnovinfar/Activity/FlashLightActivity.java
+++ app/src/main/java/com/example/amirnovinfar/Activity/FlashLightActivity.java
@@ -1,26 +1,69 @@
+
 package com.example.amirnovinfar.Activity;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
+import android.hardware.camera2.CameraManager;
+import android.media.MediaPlayer;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageButton;
+import android.widget.Toast;
+
 import com.example.amirnovinfar.R;
 
 public class FlashLightActivity extends AppCompatActivity {
-    ImageButton imageButton;
-    boolean hasflash;
+    ImageButton imageButton, img_cheshmak;
+    boolean hasflash, isflashon;
+    MediaPlayer mediaPlayer;
+    CameraManager camera;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_flash_light);
         setupviews();
         issupportedflash();
+        isflashon = false;
+
+
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.Q)
+            @Override
+            public void onClick(View view) {
+                if (isflashon==false){
+                    TurnOnFlashlights();
+                }
+
+                   else if (isflashon==true){
+                    TurnOffFlashlights();
+                }
+
+            }
+        });
+
+        img_cheshmak.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                    TurnOffFlashlights();
+                }
+            }
+        });
     }
 
     private void setupviews() {
         imageButton = findViewById(R.id.btn_switch);
+        img_cheshmak = findViewById(R.id.img_cheshmak);
     }
 
     private void issupportedflash() {
@@ -38,5 +81,37 @@ public class FlashLightActivity extends AppCompatActivity {
             alertDialog.show();
 
         }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.Q)
+    private void TurnOnFlashlights() {
+        final CameraManager cameraManager = (CameraManager) getSystemService(CAMERA_SERVICE);
+        try {
+            String camid = cameraManager.getCameraIdList()[0];
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                cameraManager.setTorchMode(camid, true);
+                isflashon = true;
+            }
+        } catch (Exception e) {
+            Toast.makeText(this, e.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+
+    @RequiresApi(api = Build.VERSION_CODES.Q)
+    private void TurnOffFlashlights() {
+        isflashon = false;
+        final CameraManager cameraManager = (CameraManager) getSystemService(CAMERA_SERVICE);
+        try {
+            String camid = cameraManager.getCameraIdList()[0];
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                cameraManager.setTorchMode(camid, false);
+            }
+        } catch (Exception e) {
+            Toast.makeText(this, e.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+
+        }
+
     }
 }
